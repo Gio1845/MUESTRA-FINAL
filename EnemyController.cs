29d1cebedf83293dd1e79b916e7cea84a6fef876
Elias2344 <eliasjamartinez@hotmail.com>
2020-04-01 08:59:36 -0700

    Proyecto

---
+++ EnemyController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float fireDelay = 1f;
+
+    public int onFireProjectileCount = 3;
+
+    private int projectilesFired = 0;
+
+    public GameObject projectile;
+
+    private float timeSinceLastFire = 0f;
+
+    private float maxPositionX = 1f;
+    private float minPositionX = 0f;
+
+    private bool movingRight = true;
+
+    public float movementSpeed = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // tamaño pantalla
+        // Screen.width
+
+        // tamaño de la nave
+        //     SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        //    float spriteWidth = sr.sprite.rect.width;
+
+        //    maxPositionX = Screen.width - (spriteWidth / 2);
+
+        //    minPositionX = 0 + (spriteWidth / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+
+        timeSinceLastFire += Time.deltaTime;
+
+        if (movingRight)
+        {
+            // move right
+            transform.Translate(new Vector2(movementSpeed, 0f));
+
+            if (pos.x >= maxPositionX)
+            {
+                // ahora se debe movel a la izquierda
+                movingRight = false;
+            }
+        }
+        else
+        {
+            // move left
+            transform.Translate(new Vector2(-movementSpeed, 0f));
+
+            if (pos.x <= minPositionX)
+            {
+                movingRight = true;
+            }
+        }
+
+        if (timeSinceLastFire >= fireDelay && projectilesFired < onFireProjectileCount)
+        {
+            Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, 100)));
+            projectile.GetComponent<Projectile>().damagebleTargetTag = "Player";
+
+            projectilesFired++;
+
+            if(projectilesFired >= onFireProjectileCount)
+            {
+                timeSinceLastFire = 0f;
+                projectilesFired = 0;
+            }
+        }
+    }
+}
+
---
+++ Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float ProjectileSpeed = 0.3f;
+
+    public int maxXPosition = 102;
+
+    public string damagebleTargetTag = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.Translate(new Vector2(0.4f, 0f));
+        if (transform.position.x > 102){
+            Destroy(gameObject);
+    }
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log(other);
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == damagebleTargetTag) {
+            Stats enemyStats = other.gameObject.GetComponent<Stats>();
+
+            enemyStats.OnHit();
+        }
+    }
+}
+
